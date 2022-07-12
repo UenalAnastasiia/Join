be9b553d291f6d633af25f1878d21d9370660c46
UenalAnastasiia <velyka.nastya@icloud.com>
2022-07-12 23:26:53 +0200

    begin task

--- js/task.js
+++ js/task.js
@@ -1,7 +1,30 @@
+let allTasks = [];
+
+
 /* 
  * Load the calendar in Add-Task-Form with min date current day 
  */
 function loadDate() {
     let currentDay = new Date().toISOString().split('T')[0];
-    document.getElementById('date').setAttribute('min', currentDay);
+    document.getElementById('taskDate').setAttribute('min', currentDay);
+}
+
+
+function createTask() {
+    let title = document.getElementById('taskTitle').value;
+    let category = document.getElementById('taskCategory');
+    let description = document.getElementById('taskDescription').value;
+    let date = document.getElementById('taskDate');
+    let urgency = document.getElementById('taskUrgency');
+    let assignedAccount = document.getElementById('taskAssignedAccount');
+    let newTask = {
+            'title': title,
+            'category': category,
+            'description': description,
+            'date': date,
+            'urgency': urgency,
+            'assignedAccount': assignedAccount
+        }
+        allTasks.push(newTask);
+        console.log(allTasks);
 }
