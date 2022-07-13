2210899afe89f28bee66834753957df5a5b73ce8
UenalAnastasiia <velyka.nastya@icloud.com>
2022-07-13 23:16:06 +0200

    task try

--- js/task.js
+++ js/task.js
@@ -12,19 +12,19 @@ function loadDate() {
 
 function createTask() {
     let title = document.getElementById('taskTitle').value;
-    let category = document.getElementById('taskCategory');
+    let category = document.getElementById('taskCategory').value;
     let description = document.getElementById('taskDescription').value;
-    let date = document.getElementById('taskDate');
-    let urgency = document.getElementById('taskUrgency');
-    let assignedAccount = document.getElementById('taskAssignedAccount');
+    let date = document.getElementById('taskDate').value;
+    let urgency = document.getElementById('taskUrgency').value;
+    let assignedAccount = document.getElementById('taskAssignedAccount').value;
     let newTask = {
-            'title': title,
-            'category': category,
-            'description': description,
-            'date': date,
-            'urgency': urgency,
-            'assignedAccount': assignedAccount
-        }
-        allTasks.push(newTask);
-        console.log(allTasks);
+        'title': title,
+        'category': category,
+        'description': description,
+        'date': date,
+        'urgency': urgency,
+        'assignedAccount': assignedAccount
+    }
+    allTasks.push(newTask);
+    console.log(allTasks);
 }
