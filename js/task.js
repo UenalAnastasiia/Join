bb861a734ee4ac3a9ea11a1da920eb38d3d04896
UenalAnastasiia <velyka.nastya@icloud.com>
2022-07-14 12:08:56 +0200

    create task and clean form

--- js/task.js
+++ js/task.js
@@ -11,12 +11,18 @@ function loadDate() {
 
 
 function createTask() {
+    
     let title = document.getElementById('taskTitle').value;
-    let category = document.getElementById('taskCategory').value;
+    let category = document.querySelector('.taskCategory').value;
     let description = document.getElementById('taskDescription').value;
     let date = document.getElementById('taskDate').value;
-    let urgency = document.getElementById('taskUrgency').value;
+    let urgency = document.querySelector('.taskUrgency').value;
     let assignedAccount = document.getElementById('taskAssignedAccount').value;
+    generateNewTask(title, category, description, date, urgency, assignedAccount);
+}
+
+
+function generateNewTask(title, category, description, date, urgency, assignedAccount) {
     let newTask = {
         'title': title,
         'category': category,
@@ -27,4 +33,12 @@ function createTask() {
     }
     allTasks.push(newTask);
     console.log(allTasks);
+    cleanTaskForm();
+}
+
+
+function cleanTaskForm() {
+    document.getElementById('taskTitle').value = '';
+    document.getElementById('taskDate').value = '';
+    document.getElementById('taskDescription').value = '';
 }
