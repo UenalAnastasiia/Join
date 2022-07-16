8d73a3dc857db43566e1f2fe6bfae6cc3bc25d94
Leon Groschek <leon.groschek12@gmail.com>
2022-07-16 14:45:10 +0200

    continue help task and click to copy function in backlog

--- js/backlog.js
+++ js/backlog.js
@@ -1,12 +1,19 @@
-"use strict"
+"use strict";
 
 function openDetails() {
-    document.body.innerHTML += openDetailsHTML();
-    let showDetails = document.getElementById('backlogDetailsContainer');
-    showDetails.classList.remove('d-none');
+  document.body.innerHTML += openDetailsHTML();
+  let showDetails = document.getElementById("backlogDetailsContainer");
+  showDetails.classList.remove("d-none");
 }
 
 function closeDetails() {
-    let showDetails = document.getElementById('backlogDetailsContainer');
-    showDetails.classList.add('d-none');
+  let showDetails = document.getElementById("backlogDetailsContainer");
+  showDetails.classList.add("d-none");
+}
+
+function clickToCopy() {
+  let email = document.getElementById("copyEmail").innerText;
+  email.textContent;
+  navigator.clipboard.writeText(email);
+  console.log(email);
 }
