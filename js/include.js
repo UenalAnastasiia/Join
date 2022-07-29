643a07baa5f488d99ea03e918016156f897dadd1
UenalAnastasiia <velyka.nastya@icloud.com>
2022-07-29 21:21:56 +0200

    responsive menu

--- js/include.js
+++ js/include.js
@@ -3,7 +3,7 @@ async function loadNavBar() {
 }
 
 
- async function includeHTML() {
+async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
         const element = includeElements[i];
@@ -15,4 +15,19 @@ async function loadNavBar() {
             element.innerHTML = 'Page not found';
         }
     }
+}
+
+
+/* 
+ * Responsive Menu
+ */
+function openResponsiveMenu() {
+    document.getElementById('menu-links').classList.remove('d-none');
+    document.getElementById('resp-header').classList.add('d-none')
+    
+}
+
+function closeResponsiveMenu() {
+    document.getElementById('menu-links').classList.add('d-none');
+    document.getElementById('resp-header').classList.remove('d-none') 
 }
